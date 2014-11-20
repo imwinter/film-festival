d02066c1247bfebe749c29b19d3e6ca92ac9537c
camargo <camargo.cac@gmail.com>
2014-11-20 14:21:59 -0800

    moved video player logic to the controller

--- client/scripts/controllers/player.js
+++ client/scripts/controllers/player.js
@@ -1,3 +1,8 @@
 angular.module('app').controller('PlayerCtrl', ['$scope', function ($scope) {
-    // TODO
+    var url = "http://dash.edgesuite.net/dash264/TestCases/1c/qualcomm/2/MultiRate.mpd";
+    var context = new Dash.di.DashContext();
+    var player = new MediaPlayer(context);
+    player.startup();
+    player.attachView(document.querySelector("#video-player"));
+    player.attachSource(url);
 }]);
--- client/scripts/dash/video.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    var url = "http://dash.edgesuite.net/dash264/TestCases/1c/qualcomm/2/MultiRate.mpd";
-    var context = new Dash.di.DashContext();
-    var player = new MediaPlayer(context);
-    player.startup();
-    player.attachView(document.querySelector("#video-player"));
-    player.attachSource(url);
-})();
